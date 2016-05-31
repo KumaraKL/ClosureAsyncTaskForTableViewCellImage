5b1e89fa2b376d7a1a650c184df67c99ee3190f9
Kumar <Kumar@techM-2.local>
2016-05-31 16:03:49 +0530

    added sample project

---
+++ ClosureAsyncTask/testCell.swift
@@ -0,0 +1,24 @@
+//
+//  testCell.swift
+//  ClosureAsyncTask
+//
+//  Created by Kumar on 31/05/16.
+//  Copyright © 2016 Kumar. All rights reserved.
+//
+
+import UIKit
+
+class testCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
